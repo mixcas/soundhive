003bffb46149c0e162273b47e5151b0f278084ad
cas <cas@interglobal.vision>
2017-07-16 21:19:37 -0500

    add UploadTrack component

--- imports/ui/components/TrackUpload/TrackUpload.js
+++ imports/ui/components/TrackUpload/TrackUpload.js
@@ -1,8 +1,9 @@
 /* eslint-disable max-len, no-return-assign */
 
 import React from 'react';
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types';
-import { FormGroup, ControlLabel, Button, Alert } from 'react-bootstrap';
+import { FormGroup, ControlLabel, Button, HelpBlock, FormControl, Checkbox } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
 import validate from '../../../modules/validate';
@@ -13,6 +14,7 @@ class TrackUpload extends React.Component {
 
     this.state = {
       magnetUri: '',
+      infoHash: '',
     };
 
     this.handleFileOnChange = this.handleFileOnChange.bind(this);
@@ -26,48 +28,82 @@ class TrackUpload extends React.Component {
         file: {
           required: true,
         },
+        title: {
+          required: true,
+        },
       },
       messages: {
         file: {
           required: 'Need a file in here, Seuss.',
+        },
+        title: {
+          required: 'Need a title in here, Seuss.',
         },
       },
       submitHandler() { component.handleSubmit(); },
     });
 
   }
+  handleSubmit() {
+    const { history } = this.props;
+    const existingTrack = this.props.doc && this.props.doc._id;
+    const methodToCall = existingTrack ? 'tracks.update' : 'tracks.insert';
+    const doc = {
+      title: this.title.value.trim(),
+      magnetUri: this.state.magnetUri.trim(),
+      infoHash: this.state.infoHash.trim(),
+      genre: this.genre.value.trim(),
+      tags: this.tags.value.trim(),
+      description: this.description.value.trim(),
+    };
+
+    if (existingTrack) doc._id = existingTrack;
+
+    Meteor.call(methodToCall, doc, (error, trackId) => {
+      if (error) {
+        Bert.alert(error.reason, 'danger');
+      } else {
+        const confirmation = existingTrack ? 'Track updated!' : 'Track added!';
+        this.form.reset();
+        Bert.alert(confirmation, 'success');
+        history.push(`/track/${trackId}`);
+      }
+    });
+  }
 
   handleFileOnChange(e) {
     const component = this;
 
     var file = e.target.files[0];
+    if (file) {
+      if (component.validateMp3(file)) {
 
-    if (component.validateMp3(file)) {
+        filer.write('/seedingTracks/' + file.name, {data: file, type: file.type}, (fileEntry, fileWriter) => {
 
-      filer.write('/seedingTracks/' + file.name, {data: file, type: file.type}, (fileEntry, fileWriter) => {
+          filer.open(fileEntry.fullPath, function(file) {
+            client.seed(file, torrent => {
+              let filename = file.name;
+              let magnetUri = torrent.magnetURI;
+              let infoHash = torrent.infoHash;
 
-        filer.open(fileEntry.fullPath, function(file) {
-          debugger;
+              component.setState({magnetUri, infoHash});
 
-          client.seed(file, torrent => {
-            let magnetUri = torrent.magnetURI;
+              component.title.value = filename;
 
-            component.magnetUri.value = magnetUri;
-
-            component.setState({magnetUri});
+              Bert.alert('Torrent generated', 'success');
+            });
+          }, function(err) {
+            console.log(err);
           });
-        }, function(err) {
+
+        }, (err) => {
           console.log(err);
         });
-
-      }, (error) => {
-        console.log(err);
-      });
-    } else {
-      Bert.alert('Select an MP3 file', 'danger');
-      component.file.value = '';
+      } else {
+        Bert.alert('Select an MP3 file', 'danger');
+        component.file.value = '';
+      }
     }
-
   }
 
   validateMp3(file) {
@@ -82,6 +118,7 @@ class TrackUpload extends React.Component {
   }
 
   render() {
+    const { doc } = this.props;
     return (
       <div className='TrackUpload'>
         <form ref={form => (this.form = form)} onSubmit={event => event.preventDefault()}>
@@ -95,18 +132,59 @@ class TrackUpload extends React.Component {
               onChange={this.handleFileOnChange}
               placeholder='Select File'
             />
-
-            {this.state.magnetUri !== '' ? <Alert bsStyle="success">
-              <strong>Torrent generated!</strong> Magnet link: <a href={this.state.magnetUri}>{this.state.magnetUri}</a>
-            </Alert> : ''}
-
-            <input
-              type='hidden'
-              name='magnetUri'
-              ref={magnetUri => (this.magnetUri = magnetUri)}
-            />
+            {this.state.magnetUri && <HelpBlock>Magnet link: <a href={this.state.magnetUri}>{this.state.magnetUri}</a></HelpBlock>}
           </FormGroup>
-          <Button type="submit" bsStyle="success">Add Track</Button>
+
+          { this.state.magnetUri && <div>
+            <FormGroup>
+              <ControlLabel>Title</ControlLabel>
+              <input
+                type='text'
+                className='form-control'
+                name='title'
+                ref={title => (this.title = title)}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <ControlLabel>Genre</ControlLabel>
+              <input
+                id='genre'
+                name='genre'
+                ref={genre => (this.genre = genre)}
+                className='form-control'
+                type='text'
+                label='Genre'
+                placeholder='Enter text'
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <ControlLabel>Tags</ControlLabel>
+              <input
+                id='tags'
+                name='tags'
+                ref={tags => (this.tags = tags)}
+                className='form-control'
+                type='text'
+                label='Tags'
+                placeholder='Comma, Separated, Tags'
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <ControlLabel>Description</ControlLabel>
+              <textarea
+                id='description'
+                name='description'
+                className='form-control'
+                ref={description => (this.description = description)}
+                label='Description'
+              ></textarea>
+            </FormGroup>
+
+            <Button type='submit' bsStyle='success'>Add Track</Button>
+          </div> }
         </form>
       </div>
     );
