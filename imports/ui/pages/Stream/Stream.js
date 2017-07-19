ca01592895da26fcd90e41ec8d19f77aea72db16
cas <cas@interglobal.vision>
2017-07-18 19:12:01 -0500

    scaffold stream page

--- imports/ui/pages/Stream/Stream.js
+++ imports/ui/pages/Stream/Stream.js
@@ -6,69 +6,47 @@ import { timeago, monthDayYearAtTime } from '@cleverbeagle/dates';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bert } from 'meteor/themeteorchef:bert';
-import TracksCollection from '../../../api/Tracks/Tracks';
+import Tracks from '../../../api/Tracks/Tracks';
 import Loading from '../../components/Loading/Loading';
 
 import './Stream.scss';
 
-const handleRemove = (documentId) => {
-  if (confirm('Are you sure? This is permanent!')) {
-    Meteor.call('documents.remove', documentId, (error) => {
-      if (error) {
-        Bert.alert(error.reason, 'danger');
-      } else {
-        Bert.alert('Document deleted!', 'success');
-      }
-    });
-  }
-};
-
-const Stream = ({ loading, documents, match, history }) => (!loading ? (
+const Stream = ({ loading, tracks, match, history }) => (!loading ? (
   <div className="Stream">
     <div className="page-header clearfix">
       <h4 className="pull-left">Stream</h4>
-      <Link className="btn btn-success pull-right" to={`${match.url}/new`}>Add Document</Link>
+      <Link className="btn btn-success pull-right" to={`/upload`}>Add track</Link>
     </div>
-    {documents.length ? <Table responsive>
+    {tracks.length ? <Table responsive>
       <thead>
         <tr>
           <th>Title</th>
-          <th>Last Updated</th>
           <th>Created</th>
-          <th />
           <th />
         </tr>
       </thead>
       <tbody>
-        {documents.map(({ _id, title, createdAt, updatedAt }) => (
+        {tracks.map(({ _id, title, createdAt }) => (
           <tr key={_id}>
             <td>{title}</td>
-            <td>{timeago(updatedAt)}</td>
             <td>{monthDayYearAtTime(createdAt)}</td>
             <td>
               <Button
                 bsStyle="primary"
-                onClick={() => history.push(`${match.url}/${_id}`)}
+                onClick={() => history.push(`track/${_id}`)}
                 block
               >View</Button>
-            </td>
-            <td>
-              <Button
-                bsStyle="danger"
-                onClick={() => handleRemove(_id)}
-                block
-              >Delete</Button>
             </td>
           </tr>
         ))}
       </tbody>
-    </Table> : <Alert bsStyle="warning">No documents yet!</Alert>}
+    </Table> : <Alert bsStyle="warning">No tracks yet!</Alert>}
   </div>
 ) : <Loading />);
 
 Stream.propTypes = {
   loading: PropTypes.bool.isRequired,
-  documents: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tracks: PropTypes.arrayOf(PropTypes.object).isRequired,
   match: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
 };
@@ -77,6 +55,6 @@ export default createContainer(() => {
   const subscription = Meteor.subscribe('tracks');
   return {
     loading: !subscription.ready(),
-    documents: Tracks.find().fetch(),
+    tracks: Tracks.find().fetch(),
   };
 }, Stream);
